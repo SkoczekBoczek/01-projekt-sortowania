1e21109c5f7a5620d57fd52dc815bcac2fc72366
Nikodem Witkowski <wnikodem04@gmail.com>
2025-03-15 14:24:09 +0100

    added quick sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -59,3 +59,24 @@ def heapSort(array):
     return array
 
 # Quick sort
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[i] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+    return i + 1
+
+def quickSort(array):
+    stack = [(0, len(array) -1)]
+    while stack:
+        low, high = stack.pop()
+        if low < high:
+            pi = partition(array, low, high)
+            if pi - 1 > low:
+                stack.append((low, pi - 1))
+            if pi + 1 < high:
+                stack.append((pi + 1, high))
+    return array
